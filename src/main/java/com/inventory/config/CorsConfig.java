19935d8eed8896fad8dbdd4238490967bcfec410
Chandan Jadhav <chandanjadhav1011@gmail.com>
2026-01-18 14:17:29 +0530

    Feat: After added CorsConfig of frontend url in assignment

--- src/main/java/com/inventory/config/CorsConfig.java
+++ src/main/java/com/inventory/config/CorsConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+
     @Bean
     public WebMvcConfigurer corseConfigurer() {
         return  new WebMvcConfigurer() {
